50dfd0b7149ac5e2326b4dff02179652a6b15f3b
Victor Baumann <victor.baumann@gmail.com>
2019-05-18 11:13:07 -0500

    filters

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
+from dash.dependencies import Input, Output
 import pandas as pd
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
@@ -9,13 +10,6 @@ app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 server = app.server
 
 df_earning = pd.read_csv("https://raw.githubusercontent.com/plotly/datasets/master/school_earnings.csv")
-
-
-df_complaints = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/26k-consumer-complaints.csv')
-df_complaints['datetime'] = pd.to_datetime(df_complaints['Date received'])
-df_complaints.index = df_complaints['datetime']
-df_complaints.resample('d')
-
 
 app.layout = html.Div(children=[
     html.H1(children='Hello Dash'),
@@ -35,6 +29,23 @@ app.layout = html.Div(children=[
         }
     ),
 
+    html.Label('Select Schools'),
+    dcc.Dropdown(
+        id="select-school",
+        options=[{"label": s, "value": s} for s in df_earning.School],
+        multi=True
+    ),
+    html.Label("Select range"),
+    dcc.RangeSlider(
+        id="gap-range",
+        marks={i: str(i) for i in range(0, 100, 10)},
+        min=0,
+        max=100,
+        value=[0, 100]
+    ),
+
+    html.Hr(),
+    
     dcc.Graph(
         id="school-earnings",
         figure={
@@ -48,6 +59,26 @@ app.layout = html.Div(children=[
     Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.
     ''', style={'color': 'darkred'})
 ])
+
+
+@app.callback(
+    Output(component_id='school-earnings', component_property='figure'),
+    [
+        Input(component_id='select-school', component_property='value'),
+        Input(component_id='gap-range', component_property='value'),
+    ]
+)
+def update_graph(selected_schools, gap_range):
+    df = df_earning
+    df = df[df.Gap.between(*gap_range)]
+    if selected_schools:
+        df = df[df.School.isin(selected_schools)]
+    return {
+        'data': [
+            {'x': df.School, 'y': df.Gap, 'type': 'bar'}
+        ]
+    }
+
 
 if __name__ == '__main__':
     app.run_server(debug=True)
